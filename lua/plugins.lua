8899c31b6fdd095badadc2a03df0197900e1f2c9
HuuPT <huu.phan@gotecq.com>
2023-07-03 21:47:58 +0700

    init 2

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -99,39 +99,10 @@ function M.setup()
       end,
     }
 
-    -- Better Comment
-    use {
-      "numToStr/Comment.nvim",
-      keys = { "gc", "gcc", "gbc" },
-      config = function()
-        require("Comment").setup {}
-      end,
-    }
 
     -- Better surround
     use { "tpope/vim-surround", event = "InsertEnter" }
 
-    -- Motions
-    use { "andymass/vim-matchup", event = "CursorMoved" }
-    use { "wellle/targets.vim", event = "CursorMoved" }
-    use { "unblevable/quick-scope", event = "CursorMoved", disable = false }
-    use { "chaoren/vim-wordmotion", opt = true, fn = { "<Plug>WordMotion_w" } }
-
-    use {
-      "phaazon/hop.nvim",
-      cmd = { "HopWord", "HopChar1" },
-      config = function()
-        require("hop").setup {}
-      end,
-      disable = true,
-    }
-    use {
-      "ggandor/lightspeed.nvim",
-      keys = { "s", "S", "f", "F", "t", "T" },
-      config = function()
-        require("lightspeed").setup {}
-      end,
-    }
 
     -- Markdown
     use {
@@ -207,67 +178,6 @@ function M.setup()
       end,
     }
 
-    -- User interface
-    use {
-      "stevearc/dressing.nvim",
-      event = "BufEnter",
-      config = function()
-        require("dressing").setup {
-          select = {
-            backend = { "telescope", "fzf", "builtin" },
-          },
-        }
-      end,
-      disable = true,
-    }
-    use { "nvim-telescope/telescope.nvim", module = "telescope", as = "telescope" }
-
-    -- Completion
-    use {
-      "ms-jpq/coq_nvim",
-      branch = "coq",
-      event = "InsertEnter",
-      opt = true,
-      run = ":COQdeps",
-      config = function()
-        require("config.coq").setup()
-      end,
-      requires = {
-        { "ms-jpq/coq.artifacts", branch = "artifacts" },
-        { "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
-      },
-      disable = true,
-    }
-
-    use {
-      "hrsh7th/nvim-cmp",
-      event = "InsertEnter",
-      opt = true,
-      config = function()
-        require("config.cmp").setup()
-      end,
-      wants = { "LuaSnip" },
-      requires = {
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lua",
-        "ray-x/cmp-treesitter",
-        "hrsh7th/cmp-cmdline",
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-calc",
-        "f3fora/cmp-spell",
-        "hrsh7th/cmp-emoji",
-        {
-          "L3MON4D3/LuaSnip",
-          wants = "friendly-snippets",
-          config = function()
-            require("config.luasnip").setup()
-          end,
-        },
-        "rafamadriz/friendly-snippets",
-        disable = false,
-      },
-    }
 
     -- Auto pairs
     use {
@@ -279,23 +189,7 @@ function M.setup()
       end,
     }
 
-    -- Auto tag
-    use {
-      "windwp/nvim-ts-autotag",
-      wants = "nvim-treesitter",
-      event = "InsertEnter",
-      config = function()
-        require("nvim-ts-autotag").setup { enable = true }
-      end,
-    }
 
-    -- End wise
-    use {
-      "RRethy/nvim-treesitter-endwise",
-      wants = "nvim-treesitter",
-      event = "InsertEnter",
-      disable = false,
-    }
 
     -- Bootstrap Neovim
     if packer_bootstrap then
